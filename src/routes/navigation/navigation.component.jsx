13392c4ad05149dd768a23a5bf541f5b4e076c57
mmalekabs <mhd_malek@outlook.sa>
2023-09-21 01:44:48 +0300

    update navigation route

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import {Fragment} from "react";
 import {Link, Outlet} from "react-router-dom";
+import {ReactComponent as PenguinLogo} from "../../assets/penguin.svg";
 
+import './navigation.styles.scss'
 const Navigation = () => {
     return (
         <Fragment>
             <div className='navigation'>
                 <Link className='logo-container' to='/'>
-                    <div>Logo</div>
+                    <PenguinLogo className='logo' style={{width: 50, height: 50}}/>
                 </Link>
                 <div className='nav-links-container'>
                     <Link className='nav-link' to='/shop'>
